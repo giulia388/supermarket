ca98d221af246d4bb4977467b827cb7339a7d37d
giulia388 <giuliacolucci388@gmail.com>
2025-09-22 23:39:48 +0200

    Aggiunta branch homepage

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -9,5 +9,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
   },
+
 ];
