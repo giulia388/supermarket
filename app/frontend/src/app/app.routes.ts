e216ed52a59b0f1bf7db245282dc9be00ffc5aa5
giulia388 <giuliacolucci388@gmail.com>
2025-09-23 19:24:14 +0200

    Aggiunta collegamento prenotazione

--- app/frontend/src/app/app.routes.ts
+++ app/frontend/src/app/app.routes.ts
@@ -4,15 +4,16 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('./pages/home/home.page').then(m => m.HomePage) // 👈 usa solo pages/home
+      import('./pages/home/home.page').then(m => m.HomePage),
+  },
+  {
+    path: 'prenotazione',
+    loadComponent: () =>
+      import('./pages/prenotazione/prenotazione.page').then(m => m.PrenotazionePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
-    path: 'prenotazione',
-    loadComponent: () => import('./pages/prenotazione/prenotazione.page').then( m => m.PrenotazionePage)
   }
-
 ];
